94ca5b9b982d6beed139edb9a00dabdc1e673bbb
Copeland-Jacob <148981842+Copeland-Jacob@users.noreply.github.com>
2024-10-26 11:03:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const minecraft = document.getElementById("minecraft");
+
+minecraft.addEventListener("load", function(){
+    minecraft.contentWindow.focus();
+})
+
+minecraft.addEventListener("click", function(){
+    minecraft.contentWindow.focus();
+})
+
+const retrobowl = document.getElementById("retrobowl");
+
+retrobowl.addEventListener("load", function(){
+    retrobowl.contentWindow.focus();
+})
+
+retrobowl.addEventListener("click", function(){
+    retrobowl.contentWindow.focus();
+})
+
+const suroi = document.getElementById("suroi");
+
+suroil.addEventListener("load", function(){
+    suroi.contentWindow.focus();
+})
+
+suroi.addEventListener("click", function(){
+    suroi.contentWindow.focus();
+})
+
+const slope = document.getElementById("slope");
+
+slope.addEventListener("load", function(){
+    suroi.contentWindow.focus();
+})
+
+slope.addEventListener("click", function(){
+    suroi.contentWindow.focus();
+})
